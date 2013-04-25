6a965736474141fb1fc535fc59377e73ddbfb4f3
Sam <samprofessional@gmail.com>
2013-04-25 16:57:34 -0700

    Added create post page

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,7 @@
 require 'faker'
 
-5.times do
-  Post.create(:title => Faker::Company.catch_phrase, :content => Faker::Lorem.paragraph)
-end
+# 5.times do
+#   Post.create(:title => Faker::Company.catch_phrase, :content => Faker::Lorem.paragraph)
+# end
+
+
