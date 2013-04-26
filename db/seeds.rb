1c80a576acbf24477a4c6830332ca766d3c029ad
Sam <samprofessional@gmail.com>
2013-04-25 19:14:25 -0700

    Added tags functionality

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,5 +3,9 @@ require 'faker'
 # 5.times do
 #   Post.create(:title => Faker::Company.catch_phrase, :content => Faker::Lorem.paragraph)
 # end
+tags = Tag.all
+
+Post.all.each { |post| post.tags << tags.sample }
+
 
 
